3bbfc065ccd31bb3c06a5ff1bfe2f186a7abae50
Nguyễn Hoàng Thiên Bảo <thienbaonguyen276@gmail.com>
2026-09-17 13:31:39 +0700

    ViewModel
    #15 ViewModel

---
+++ ViewModels/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace THUCTAP.ViewModels
+{
+    public class LoginRequest
+    {
+        public string userName { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
