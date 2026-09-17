d2dafd09032c579f0a1377a9367ae9e72f7f6d51
Nguyễn Hoàng Thiên Bảo <thienbaonguyen276@gmail.com>
2026-09-17 13:22:49 +0700

    Interface
    #5 Interface

---
+++ Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using THUCTAP.ViewModels;
+
+namespace THUCTAP.Interfaces
+{
+    public interface IAuthService
+    {
+        LoginResponse? Authenticate(LoginRequest request);
+    }
+}
---
+++ Interfaces/ICustomerMasterService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using THUCTAP.ViewModels;
+
+namespace THUCTAP.Interfaces
+{
+    public interface ICustomerMasterService
+    {
+        Task<PagedResult<CustomerMasterResponseDto>>GetAllAsync(CustomerMasterFilterRequest filter);
+        Task<CustomerMasterResponseDto>CreateAsync(CustomerMasterRequest request);
+        Task<CustomerMasterResponseDto?>UpdateAsync(int id, CustomerMasterRequest request);
+        Task<bool>DeleteAsync(int id);
+        Task<int> ImportExcelAsync(IFormFile file);
+    }
+}
