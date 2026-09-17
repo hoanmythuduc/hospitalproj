594553ee1e00c8897f023f888f237904fd8f3507
Nguyễn Hoàng Thiên Bảo <thienbaonguyen276@gmail.com>
2026-09-17 13:28:58 +0700

    Model
    #10 Model

---
+++ Models/AppAction.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace THUCTAP.Models
+{
+    [Table("Action")]
+    public class AppAction : BaseModel
+    {
+        [JsonPropertyName("menuId")]
+        public int menuId { get; set; }
+
+        [JsonIgnore]
+        [ForeignKey(nameof(menuId))]
+        public Menu? menu { get; set; }
+
+        [JsonPropertyName("label")]
+        public string label { get; set; } = string.Empty;
+
+        [JsonPropertyName("code")]
+        public string code { get; set; } = string.Empty;
+
+        [JsonPropertyName("endpoint")]
+        public string endpoint { get; set; } = string.Empty;
+
+        [JsonPropertyName("method")]
+        public string method { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public ICollection<Group> group { get; set; } = new List<Group>();
+
+    }
+}
---
+++ Models/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace THUCTAP.Models
+{
+    [Table("Menu")]
+    public class Menu : BaseModel
+    {
+        [JsonPropertyName("to")]
+        public string to { get; set; } = string.Empty;
+
+        [JsonPropertyName("parentId")]
+        public int? parentId { get; set; } 
+
+        [JsonIgnore]
+        [ForeignKey(nameof(parentId))]
+        public Menu? parent { get; set; }
+
+        [JsonPropertyName("children")]
+        public ICollection<Menu> children { get; set; } = new List<Menu>();
+
+        [JsonPropertyName("label")]
+        public string label { get; set; } = string.Empty;
+
+        [JsonPropertyName("icon")]
+        public string icon { get; set; } = string.Empty;
+
+        [JsonPropertyName("action")]
+        public ICollection<AppAction> action { get; set; } = new List<AppAction>();
+
+        [JsonIgnore]
+        public ICollection<Group> group { get; set; } = new List<Group>();
+        public ICollection<FormField> formFields { get; set; } = new List<FormField>();
+    }
+}
